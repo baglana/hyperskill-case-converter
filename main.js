cf78a6e47ff4e4adb2f64988c1976eadde891aea
baglana <102351104+baglana@users.noreply.github.com>
2023-08-19 22:23:29 +0600

    Finish 3/3: Save me

--- main.js
+++ main.js
@@ -1,35 +1,53 @@
-let textarea = document.querySelector("textarea");
+let textArea = document.querySelector("textarea");
 let upperCaseBtn = document.getElementById("upper-case");
 let lowerCaseBtn = document.getElementById("lower-case");
 let properCaseBtn = document.getElementById("proper-case");
 let sentenceCaseBtn = document.getElementById("sentence-case");
+let saveTxtFileBtn = document.getElementById("save-text-file");
 
 
 upperCaseBtn.addEventListener("click", () => {
-  textarea.value = textarea.value.toUpperCase();
+  textArea.value = textArea.value.toUpperCase();
 });
 
 lowerCaseBtn.addEventListener("click", () => {
-  textarea.value = textarea.value.toLowerCase();
+  textArea.value = textArea.value.toLowerCase();
 });
 
 properCaseBtn.addEventListener("click", () => {
-  let srcText = textarea.value.toLowerCase();
+  let srcText = textArea.value.toLowerCase();
   let tokens = srcText.split(" ");
   for (i in tokens) {
     tokens[i] = tokens[i].substring(0, 1).toUpperCase()
       + tokens[i].substring(1);
   }
-  textarea.value = tokens.join(" ");
+  textArea.value = tokens.join(" ");
 });
 
 sentenceCaseBtn.addEventListener("click", () => {
-  let srcText = textarea.value.toLowerCase();
+  let srcText = textArea.value.toLowerCase();
   let tokens = srcText.split(". ");
   for (i in tokens) {
     tokens[i] = tokens[i].substring(0, 1).toUpperCase()
       + tokens[i].substring(1);
   }
-  textarea.value = tokens.join(". ");
+  textArea.value = tokens.join(". ");
 });
 
+saveTxtFileBtn.addEventListener("click", () => {
+  let filename = "text.txt";
+  download(filename, textArea.value);
+});
+
+function download(filename, text) {
+  let element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
